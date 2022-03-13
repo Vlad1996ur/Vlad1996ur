8d799849aad7a6155e4a22a97cb176ba15fd9eb7
Vlad1996ur <101491585+Vlad1996ur@users.noreply.github.com>
2022-03-13 10:41:42 +0500

    Add files via upload

---
+++ tok.py
@@ -0,0 +1,20 @@
+import vk_api, time
+from vk_api.longpoll import VkLongPoll, VkEventType
+import vk
+
+
+token ="c063e76ac3008240b9a5d941fb5b6a0165d2e8d7973c4a987faa633a85374989188a7f68c86ddd2e3d50c"
+
+vk_session = vk_api.VkApi(token=token)
+api = vk_session.get_api()
+t = api.messages.getChat(chat_id="61312")
+p = (t["users"])
+q = str(p)
+users=(q[1:-1])
+
+titl="♡♡♡"
+
+
+
+tam=1800
+print(users)
